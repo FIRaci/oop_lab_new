e1a5a47e9c27ca7c788a1fe0baaea13d7ccaed84
unknown <bequocthanh2882005@gmail.com>
2025-03-21 14:33:52 +0700

    La02

---
+++ Lab02/AimsProject/src/Aims.java
@@ -0,0 +1,23 @@
+public class Aims {
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 124, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", "John Musker", 90, 18.99f);
+
+        cart.addDigitalVideoDisc(dvd1);
+        cart.addDigitalVideoDisc(dvd2);
+        cart.addDigitalVideoDisc(dvd3);
+
+        System.out.println("\nCart before removing:");
+        cart.printCart();
+        System.out.println("Total cost: $" + cart.totalCost());
+        
+        cart.removeDigitalVideoDisc(dvd2);
+        
+        System.out.println("\nCart after removing:");
+        cart.printCart();
+        System.out.println("Total cost after removal: $" + cart.totalCost());
+    }
+}
